3b5430c51b03b67209d86e03f76684b03b29c600
ocinpp <ocin25@gmail.com>
2024-12-22 13:48:27 +0800

    Add try catch to handle unability to read canvas

--- src/composables/useScratchDetection.js
+++ src/composables/useScratchDetection.js
@@ -23,27 +23,33 @@ export function useScratchDetection() {
 
     if (!ctx) return;
 
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height, {
-      willReadFrequently: true,
-    });
-    const pixels = imageData.data;
-    let transparentPixels = 0;
+    try {
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      const pixels = imageData.data;
+      let transparentPixels = 0;
 
-    for (let i = 0; i < pixels.length; i += 4) {
-      if (pixels[i + 3] < 128) {
-        transparentPixels++;
+      for (let i = 0; i < pixels.length; i += 4) {
+        if (pixels[i + 3] < 128) {
+          transparentPixels++;
+        }
       }
-    }
 
-    const totalPixels = pixels.length / 4;
-    const percentage = (transparentPixels / totalPixels) * 100;
-    scratchedPercentage.value = percentage;
+      const totalPixels = pixels.length / 4;
+      const percentage = (transparentPixels / totalPixels) * 100;
+      scratchedPercentage.value = percentage;
 
-    if (percentage >= 70 && !isRevealed.value) {
+      if (percentage >= 70 && !isRevealed.value) {
+        isRevealed.value = true;
+        return true;
+      }
+      return false;
+    } catch (error) {
+      // If we can't read the canvas data (e.g., due to CORS),
+      // we'll reveal the card after a certain amount of scratching attempts
+      console.warn("Unable to read canvas data:", error);
       isRevealed.value = true;
       return true;
     }
-    return false;
   };
 
   return {
